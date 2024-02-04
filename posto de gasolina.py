1219fd06db726fa7040be432d6348f4eff0605ed
Gabriel Vitor <90521193+Gabriel21Oliver@users.noreply.github.com>
2024-02-04 12:21:15 -0300

    Create posto de gasolina.py

---
+++ posto de gasolina.py
@@ -0,0 +1,11 @@
+## se o valor for menor quer 40 reais sera melhor coloca alcool
+
+
+num = float(input('quanto ira por de gasolina?'))
+num1 = float( input('insira o valor da gasolina: '))
+if(num*0.7>40):
+  print("Sera viavel pois voce rodara mais tempo")
+elif(num<40):
+  print('Como rodara pouco tempo compensa voce roda com Alcool')
+else:
+    print('numero igual que zero')
